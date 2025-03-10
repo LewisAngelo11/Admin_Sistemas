4cf2b9e1e1a5b8a8102f873b942bdcc94bbd7ab6
Luis Angel Soto Galvez <152150198+LewisAngelo11@users.noreply.github.com>
2025-03-10 01:52:38 -0700

    Avances de Ubuntu

--- HTTP/Ubuntu/Funciones_HTTP.sh
+++ HTTP/Ubuntu/Funciones_HTTP.sh
@@ -8,15 +8,15 @@ get_last_lts_version(){
 
     # Extraer las URLs de las versiones de Apache del html de descargas
     local versions
-    versions=$(echo "$html" | grep -oP 'httpd-\d+\.\d+\.\d+\.tar\.gz')
+    versions=$(curl -s "$url" | grep -oE 'httpd-[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz')
 
     # Filtrar las versiones LTS (pares en el número menor de la versión)
     local lts_versions
-    lts_versions=$(echo "$versions" | grep -E 'httpd-\d+\.(2|4|6|8)\.' )
+    lts_versions=$(curl -s "$versions" | grep -E 'httpd-\d+\.(2|4|6|8)\.' )
 
     # Obtener la última versión LTS (más reciente)
     local latest_lts_version
-    latest_lts_version=$(echo "$lts_versions" | sort -V | tail -1)
+    latest_lts_version=$(curl -s "$lts_versions" | sort -V | tail -1)
 
     # Imprimir la última versión LTS
     echo "$latest_lts_version"
@@ -26,7 +26,46 @@ instalar_servidor_http(){
     
 }
 
+get_html(){
+    # Obtener el HTML de la página de descargas de Apache
+    local url=$1
+    local html
+    html=$(curl -s "$url")
+    echo "${html}"
+}
 
 
+get_last_lts_version(){
+    local url=$1
+
+    # Filtrar las versiones LTS (pares en el número menor de la versión)
+    local lts_versions=$(curl -s "$url" | grep -oE 'httpd-[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz' | grep -E 'httpd-[0-9]+\.(2|4|6|8)\.' | sort -V | tail -1)
+
+    echo "${lts_versions}"
+}
 
 
+install_server_http(){
+    local url=$1
+    local versionInstall=$2
+    local archivoDescomprimido=$3
+    local servicio=$4
+
+    # Instalar la version
+    curl -s -O "$url/httpd-$versionInstall"
+    # Descomprimir el acrichivo descarado
+    sudo tar -xzf $versionInstall > /dev/null 2>&1
+    # Entrar a la carpeta
+    cd "$archivoDescomprimido"
+    # Compilar el archivo
+    ./configure --prefix=/usr/local/"$servicio" > /dev/null 2>&1
+    # Instalar servicio
+    make
+    sudo make install
+
+}
+
+remove_tar_gz_suffix() {
+    local filename=$1
+    echo "${filename%.tar.gz}"
+}
--- HTTP/Ubuntu/main_HTTP.sh
+++ HTTP/Ubuntu/main_HTTP.sh
@@ -14,8 +14,47 @@ while [ "$OPCION" -ne 0 ]; do
 
     case "$OPCION" in
         1)
-            downloadsApache="https://httpd.apache.org/download.cgi"
+            downloadsApache="https://downloads.apache.org/httpd/"
+            #page_apache=$(get_html "$downloadsApache")
+            #all_versions=$(get_all_versions "$downloadsApache")
+            #all_lts_versions=$(get_all_lts_versions "$downloadsApache")
             last_lts_version=$(get_last_lts_version "$downloadsApache")
+            clean_version=$(remove_tar_gz_suffix "$last_lts_version")
+            echo "Versiones: $all_versions"
+            echo "Versiones LTS: $all_lts_versions"
+            echo "Version LTS mas recente: $last_lts_version"
+
+            echo "¿Que versión de apache desea instalar"
+            echo "1. Última versión LTS "
+            echo "2. Versión de desarrollo"
+            echo "0. Salir"
+            read -p "Eliga una opción: " OPCION_APACHE
+
+            case "$OPCION_APACHE" in
+                "1")
+                    read -p "Ingrese el puerto en el que se instalará Apache: " PORT
+                    
+                    if ss -tuln | grep -q ":$PORT "; then
+                        echo "El puerto $PORT esta en uso. Eliga otro."
+                    else 
+                        instalar_servidor_http "$downloadsApache" "$last_lts_version" "$clean_version" "apache"
+                        # Verificar la instalacón
+                        /usr/local/apache2/bin/httpd -v
+                        # Ruta de la configuración del archivo
+                        routeFileConfiguration="/usr/local/apache2/conf/httpd.conf"
+                        # Remover el puerto en uso actual
+                        sudo sed -i '/^Listen/d' $routeFileConfiguration
+                        # Añadir puerto que eligio el usuario
+                        sudo printf "Listen $PORT" >> $routeFileConfiguration
+                        # Comprobar si el puerto esta escichando
+                        sudo grep -i "Listen $puerto" $routeFileConfiguration
+                    fi
+                    ;;
+                *)
+                    echo "Saliendo..."
+                    break
+                    ;;
+            esac
             ;;
         2)
             ;;
